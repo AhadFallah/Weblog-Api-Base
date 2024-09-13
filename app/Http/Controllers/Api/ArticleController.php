ddc1036d6064f7f03e71e27655a49073b6f26772
AhadFallah <ahad.fallah.k@gmail.com>
2024-09-13 20:58:58 +0330

    edit some small things

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -5,15 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Http\Resources\ArticleResource;
 
 class ArticleController extends Controller
 {
+    //@param cookies[category] string example '1,2,3'
     public function get(Request $request)
     {
-        //should reseve an array  of category id that user mostly clicked
+        //should receive an string "1,2" of category id that user mostly clicked
         $cookies = $request->cookie();
-        $article = Article::get_article(isset($cookies['category']) ? $cookies['category'] : null);
-        dd($article[0]->id);
+        //get article by the category most clicked by user that retrieve by cookies
+        $articles = Article::get_article(isset($cookies['category']) ? $cookies['category'] : null);
+        //make the valid data for response
+        $articles = ArticleResource::collection($articles);
+
+        return response()->json([
+            'articles' => $articles
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $request->validated();
 
     }
 }
